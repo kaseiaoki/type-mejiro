bd1cdae8757aa491be65c4189749593a23f6b1a6
aucuba <aoki.11235813@ezweb.ne.jp>
2018-03-15 01:39:55 +0900

    プロトタイプのts化

---
+++ tutorial-typescript/src/main.ts
@@ -0,0 +1,62 @@
+let qword : Array<string> = ["title"];
+let text :Array<string> = [];
+let index :number = 1;
+
+class EventName {
+    static LOAD:string = "load";
+    static CLICK:string = "click";
+    static MOUSE_MOVE:string = "mousemove";
+}
+
+// EventNameクラスを使用してイベント名を指定する。
+// window.addEventListener(EventName.LOAD, () => {alert("ウィンドウのロードが完了しました。")});
+// window.addEventListener(EventName.CLICK, () => {alert("clickされました。");});
+// window.addEventListener(EventName.MOUSE_MOVE, () => {console.log("マウス移動しています。");});
+
+export interface IWindow extends Window {
+  webkitSpeechRecognition: any;
+}
+const {webkitSpeechRecognition} : IWindow = <IWindow>window;
+const recognition = new webkitSpeechRecognition();
+
+const rec = document.getElementById('speak');
+
+rec.addEventListener(EventName.CLICK, function () {
+  recognition.start();
+  console.log("recording");
+});
+
+const next = document.getElementById('nextbutton');
+
+next.addEventListener(EventName.CLICK, function () {
+  index+=1;
+  document.getElementById("question_index").innerHTML=""+index+"";
+  document.getElementById("now_question").innerHTML=qword[index];
+});
+
+const doc = document.getElementById('wordbutton');
+doc.addEventListener(EventName.CLICK, function () {
+  let question: string  =  (<HTMLInputElement>document.getElementById("q_area")).value;
+  qword.push(question);
+  console.log(question);
+});
+
+const answer = document.getElementById('cansbutton');
+answer.addEventListener(EventName.CLICK, function () {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = qword[index];
+    msg.lang = 'en-US';
+    msg.rate = 0.5;
+    speechSynthesis.speak(msg);
+});
+
+recognition.lang = 'en-US';
+recognition.addEventListener('result', function(event){
+  let text: string= event.results.item(0).item(0).transcript;
+  console.log(text);
+  if(text==qword[1]){
+    document.getElementById("result").innerHTML="<li class='fa fa-circle-o'>Correct</li>";
+  }else{
+      document.getElementById("result").innerHTML="<li class='fa fa-circle-o'>Correct</li>";
+  }
+}, false);
